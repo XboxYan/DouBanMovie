7a5378d1d51cc61d2c46e85c0d9d38d5e80d3d7b
XboxYan <yanwenbin1991@live.com>
2017-08-11 18:03:28 +0800

    sectionList

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,7 @@ import {
     Text,
     StyleSheet,
     ScrollView,
+    SectionList,
     UIManager,
     LayoutAnimation,
     Image,
@@ -19,7 +20,6 @@ import {
 import TabItem from '../compoents/TabItem';
 import AppTop from '../compoents/AppTop';
 import Loading from '../compoents/Loading';
-import MovieList from '../compoents/MovieList';
 import Swiper from '../compoents/Swiper';
 
 import fetchData from '../util/Fetch';
@@ -29,7 +29,8 @@ import _ from '../theme';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const MovieTitle = observer((props) => (
-    <View style={[styles.view_hd, { borderColor: _.Color }]}>
+    <View style={styles.view_hd}>
+        <View style={[styles.line, { backgroundColor: _.Color }]} />
         <Text style={styles.view_title}>{props.title}</Text>
         <TouchableOpacity
             disabled={!!!props.title}
@@ -43,6 +44,30 @@ const MovieTitle = observer((props) => (
     </View>
 ))
 
+const MovieItem = (props) => (
+    <TouchableOpacity
+        activeOpacity={.8}
+        onPress={() => props.navigation.navigate('MovieDetail', { movieId: props.item.movieId })}
+        style={styles.movieitem}>
+        <Image
+            style={styles.movieimg}
+            source={{ uri: props.item.img }}
+            defaultSource={require('../img/img_place.png')}
+        />
+        <View style={styles.movietext}>
+            <Text numberOfLines={1} style={styles.moviename}>{props.item.name}</Text>
+        </View>
+    </TouchableOpacity>
+)
+
+const MovieList = (props) => (
+    <View style={styles.movielist}>
+        {
+            props.data.map((item, i) => <MovieItem key={item.movieId} item={item} navigation={props.navigation} />)
+        }
+    </View>
+)
+
 const BannerItem = observer((props) => (
     <TouchableOpacity
         activeOpacity={.9}
@@ -50,7 +75,7 @@ const BannerItem = observer((props) => (
         style={styles.banner}
     >
         <Image style={styles.bannerimg} source={{ uri: props.data.img }} />
-        <Text style={styles.bannertext}>{props.data.desc||' '}</Text>
+        <Text style={styles.bannertext}>{props.data.desc || ' '}</Text>
     </TouchableOpacity>
 ))
 
@@ -77,6 +102,14 @@ export default class Home extends PureComponent {
         return this.data.viewItemModels || [{}, {}, {}, {}, {}, {}];
     }
 
+    @computed get sections() {
+        return this.viewItemModels.map((el, i) => ({
+            data: [el.videos],
+            title: el.title,
+            key: "section" + i
+        }))
+    }
+
     getHot = () => {
         fetchData('hotPlay', {
             par: {
@@ -88,7 +121,7 @@ export default class Home extends PureComponent {
                 this.data = data.body;
                 this.isRender = true;
                 LayoutAnimation.spring();
-                
+
             }
         )
     }
@@ -98,46 +131,41 @@ export default class Home extends PureComponent {
     }
 
     componentWillUpdate() {
-        //LayoutAnimation.easeInEaseOut();
+        LayoutAnimation.easeInEaseOut();
+    }
+
+    renderHeader = () => {
+        return (
+            <Swiper dotColor={_.Color} style={styles.bannerWrap}>
+                {
+                    this.bannerDatas.map((el, i) => <BannerItem navigation={this.props.navigation} data={el} key={i + el.id} />)
+                }
+            </Swiper>
+        )
     }
 
     render() {
         const { navigation } = this.props;
-        let [movie, tv, dongman, zongyi, wei, shaoer] = this.viewItemModels;
         return (
             <View style={styles.content}>
                 <AppTop title='推荐' />
-                <ScrollView style={styles.content}>
-                    <Swiper dotColor={_.Color} style={styles.bannerWrap}>
-                        {
-                            this.bannerDatas.map((el, i) => <BannerItem navigation={navigation} data={el} key={i + el.id} />)
-                        }
-                    </Swiper>
-                    <View style={styles.viewcon}>
-                        <MovieTitle title={movie.title || ''} navigation={navigation} />
-                        <MovieList isRender={this.isRender} data={movie.videos} navigation={navigation} />
-                    </View>
-                    <View style={styles.viewcon}>
-                        <MovieTitle title={tv.title || ''} navigation={navigation} />
-                        <MovieList isRender={this.isRender} data={tv.videos} navigation={navigation} />
-                    </View>
-                    <View style={styles.viewcon}>
-                        <MovieTitle title={dongman.title || ''} navigation={navigation} />
-                        <MovieList isRender={this.isRender} data={dongman.videos} navigation={navigation} />
-                    </View>
-                    <View style={styles.viewcon}>
-                        <MovieTitle title={zongyi.title || ''} navigation={navigation} />
-                        <MovieList isRender={this.isRender} data={zongyi.videos} navigation={navigation} />
-                    </View>
-                    <View style={styles.viewcon}>
-                        <MovieTitle title={wei.title || ''} navigation={navigation} />
-                        <MovieList isRender={this.isRender} data={wei.videos} navigation={navigation} />
-                    </View>
-                    <View style={styles.viewcon}>
-                        <MovieTitle title={shaoer.title || ''} navigation={navigation} />
-                        <MovieList isRender={this.isRender} data={shaoer.videos} navigation={navigation} />
-                    </View>
-                </ScrollView>
+                {
+                    this.isRender ?
+                    <SectionList
+                        ListHeaderComponent={this.renderHeader}
+                        initialNumToRender={1}
+                        renderItem={({ item }) => <MovieList data={item} navigation={navigation} />}
+                        stickySectionHeadersEnabled={true}
+                        renderSectionHeader={({ section }) => <MovieTitle title={section.title} navigation={navigation} />}
+                        keyExtractor={(item, index) => "item" + index}
+                        //enableVirtualization={true}
+                        //removeClippedSubviews={false}
+                        sections={this.sections}
+                    />
+                    :
+                    <Loading/>
+                }
+
             </View>
         )
     }
@@ -147,21 +175,17 @@ const styles = StyleSheet.create({
     content: {
         flex: 1
     },
-    viewcon: {
-        marginBottom: 10,
-        backgroundColor: '#fff',
-        paddingVertical: 10,
-        minHeight: 150,
-        overflow:'hidden'
-    },
     view_hd: {
-        height: 16,
-        borderLeftWidth: 3,
+        height: 40,
         flexDirection: 'row',
         alignItems: 'center',
         paddingHorizontal: 10,
-        marginVertical: 10,
-        marginLeft: 10
+        backgroundColor: '#fff'
+    },
+    line: {
+        height: 16,
+        width: 3,
+        marginRight: 10,
     },
     view_title: {
         fontSize: 16,
@@ -177,36 +201,67 @@ const styles = StyleSheet.create({
         fontSize: 13,
         color: '#999'
     },
-    bannerWrap:{
-        backgroundColor:'#fff'
+    bannerWrap: {
+        backgroundColor: '#fff'
     },
     banner: {
         flex: 1,
         height: $.WIDTH * 9 / 16,
-        borderRadius:3,
-        backgroundColor:'#f1f1f1',
-        overflow:'hidden'
+        borderRadius: 3,
+        backgroundColor: '#f1f1f1',
+        overflow: 'hidden'
     },
     bannerimg: {
-        width:'100%',
+        width: '100%',
         height: '100%',
         resizeMode: 'cover',
-        borderRadius:3
+        borderRadius: 3
     },
-    bannertext:{
-        fontSize:16,
-        color:'#fff',
-        paddingHorizontal:10,
-        paddingVertical:7,
-        position:'absolute',
+    bannertext: {
+        fontSize: 16,
+        color: '#fff',
+        paddingHorizontal: 10,
+        paddingVertical: 7,
+        position: 'absolute',
         //backgroundColor:'rgba(0,0,0,.3)',
-        textShadowColor :'#000',
-        textShadowOffset: {width: 0, height: 2},
+        textShadowColor: '#000',
+        textShadowOffset: { width: 0, height: 2 },
         textShadowRadius: 20,
-        left:0,
-        right:0,
-        bottom:0,
+        left: 0,
+        right: 0,
+        bottom: 0,
         //borderBottomLeftRadius: 3,
         //borderBottomRightRadius: 3,
-    }
+    },
+    movielist: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingHorizontal: 5,
+        paddingTop: 10,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginBottom: 10,
+    },
+    movieitem: {
+        width: ($.WIDTH - 40) / 3,
+        marginHorizontal: 5,
+    },
+    movieimg: {
+        width: '100%',
+        height: ($.WIDTH - 40) / 2,
+        flex: 1,
+        borderRadius: 3,
+        resizeMode: 'cover'
+    },
+    movietext: {
+        alignItems: 'center',
+        height: 40,
+        flexDirection: 'row'
+    },
+    moviename: {
+        fontSize: 14,
+        color: '#333',
+        textAlign: 'center',
+        flex: 1
+    },
 })
